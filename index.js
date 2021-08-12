c66405bfdb3fa85b5ab510ebc805416bf2c67817
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-12 13:46:41 -0400

    commit tree 57e776cbcff34f48644e081544c7a853c1bf2c76

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(driverNames, string) {
+    return driverNames.filter(element => element.toLowerCase() === string.toLowerCase())
+};
+
+function  fuzzyMatch(driverNames, string) {
+    return driverNames.filter(element => element.slice(0,1) === string.slice(0,1))
+};
+
+function matchName(driverName, string) {
+    return driverName.filter(element => element.name === string);
+}
